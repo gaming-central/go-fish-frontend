9f90233a5ad34dcc45c420a9c58cf9e4c995fe1c
William Alburger <xltmxltm876@gmail.com>
2021-04-29 18:21:57 -0400

    changed the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,8 +21,25 @@ const displayValues = {
 }
 
 export const Card = ({ className = '', style = {}, suit = 'hearts', value = 1 }) => {
-  return <div className={`border rounded-lg bg-white ${className}`} style={style}>
-    {displayValues[value]}
-    {displayValues[suit]}
+  return <div className={`border-4 p-2 flex flex-col rounded-lg border-purple-900 bg-white text-3xl w-56 h-80 ${className}`} style={style}>
+    <span className=' flex items-center mr-auto flex-col'>
+      <span>
+        {displayValues[value]}
+      </span>
+      <span>
+        {displayValues[suit]}
+      </span>
+    </span>
+    <span className='m-auto text-6xl'>
+      {displayValues[suit]}
+    </span>
+    <span className='ml-auto flex items-center flex-col'>
+      <span>
+        {displayValues[suit]}
+      </span>
+      <span>
+        {displayValues[value]}
+      </span>
+    </span>
   </div>
 }
